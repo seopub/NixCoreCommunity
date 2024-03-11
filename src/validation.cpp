9cdde2a17ce87453eb2cb056bd6ac83436d41b59
seopub <142002838+seopub@users.noreply.github.com>
2024-03-11 18:08:48 +0200

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1537,8 +1537,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     // Subsidy is cut in half every 1,050,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     //On genesis, create 38 million NIX for faucet
-    if(nHeight == 1)
-        nSubsidy = 38000000 * COIN;
+    //if(nHeight == 1) nSubsidy = 38000000 * COIN;
 
     //stop halving when subsidy reaches 1 coin per block
     if(nSubsidy < (1 * COIN))
